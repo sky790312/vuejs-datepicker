0d62ae1db4f49e8e38328018ad745721f868f53b
kevin.hu <>
2019-02-15 15:15:04 +0800

    update information

--- VueDatepicker.umd.js
+++ VueDatepicker.umd.js
@@ -2368,12 +2368,12 @@ if (typeof window !== 'undefined') {
 // Indicate to webpack that this file can be concatenated
 /* harmony default export */ var setPublicPath = (null);
 
-// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"154b361e-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueDatepicker/index.vue?vue&type=template&id=54787d29&
+// CONCATENATED MODULE: ./node_modules/cache-loader/dist/cjs.js?{"cacheDirectory":"node_modules/.cache/vue-loader","cacheIdentifier":"154b361e-vue-loader-template"}!./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/cache-loader/dist/cjs.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./src/components/VueDatepicker/index.vue?vue&type=template&id=291ccc9e&
 var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"i-datepicker"},[_c('input',{directives:[{name:"model",rawName:"v-model",value:(_vm.selectedDateValue),expression:"selectedDateValue"}],staticClass:"i-datepicker-input",attrs:{"type":"date"},domProps:{"value":(_vm.selectedDateValue)},on:{"click":function($event){$event.preventDefault();_vm.onDatepickerInputClick()},"input":function($event){if($event.target.composing){ return; }_vm.selectedDateValue=$event.target.value}}}),(_vm.shouldShowDatepicker)?_c('div',{staticClass:"i-datepicker-container"},[_c('VueDatepickerHeader'),_c('VueDatepickerContent')],1):_vm._e()])}
 var staticRenderFns = []
 
 
-// CONCATENATED MODULE: ./src/components/VueDatepicker/index.vue?vue&type=template&id=54787d29&
+// CONCATENATED MODULE: ./src/components/VueDatepicker/index.vue?vue&type=template&id=291ccc9e&
 
 // EXTERNAL MODULE: ./node_modules/core-js/modules/es6.regexp.to-string.js
 var es6_regexp_to_string = __webpack_require__("6b54");
